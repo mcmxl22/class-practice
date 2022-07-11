f4f49c2291868ec51792903381a67b74801ec099
Micah M <mcmxl@reagan.com>
2022-07-11 11:32:21 -0600

    Update game.py

--- game.py
+++ game.py
@@ -1,3 +1,5 @@
+import os
+
 # Parent class.
 class Character:
     """Define player's character."""
@@ -27,19 +29,46 @@ class NPC(Character):
     species = 'Computer' # Overrides Character/parent class variable.
 
 
-def choose_name() -> str:
-    """Name your character."""
-    name = input("Enter a name. ")
-    return name
+class Player_information:
+    """get player names."""
+
+    def choose_name() -> str:
+        """Name your character."""
+        name = input("Enter a name. ")
+        return name.capitalize()
+
+    def get_computer_name() -> str:
+        """Get computer's name."""
+        name = os.environ['COMPUTERNAME']
+        return name
+
+
+class Moves:
+    """Define moves."""
+
+    def attack(self):
+        """Attack."""
+        attack = Character.hurt()
+        print(f"You attack with {Character.hurt()}.")
+
+    def defend(self):
+        """Defend."""
+        #print(f"You defend with {self.name}.")
+        pass
+
+    def heal(self):
+        """Heal."""
+        #print(f"You heal with {self.name}.")
+        pass
 
 
 def main():
     """Main function."""
     # Creates an instance of the Character object, and lets the user choose a name.
-    player = Character(choose_name())
+    player = Character(Player_information.choose_name())
     
     # Creates an instance of, and names the NPC object.
-    machine = NPC('mcmxl22') 
+    machine = NPC(Player_information.computer_name())
 
     # Uses a class method to hurt the character.
     player.hurt()
