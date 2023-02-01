e985bcd493ea7b72a59d8ce0b1da91dc05087b38
Micah McConnaughey <mcmxl@hotmail.com>
2023-02-01 10:46:22 -0700

    Added README.md. Added docstring to apple.py

--- apple.py
+++ apple.py
@@ -1,10 +1,11 @@
 class Person:
+    """A class to represent a person."""
     def __init__(self, name):
         self.name = name
 
 
 class Apple(Person):
-
+    """A class to represent an apple."""
     def get_color(self):
         color = input(f"What color is the apple? ")
         return color
@@ -14,6 +15,7 @@ class Apple(Person):
 
 
 def main():
+    """The main function."""
     a = Apple('Granny Smith')
     p = Person("Eve")
     c = Apple.get_color("")
