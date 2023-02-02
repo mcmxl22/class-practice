752c407a25e2cbdc76e3697d50a795f77c2437aa
M. McConnaughey <mcmxl@reagan.com>
2023-02-02 12:16:22 -0700

    Minor changes

--- car.py
+++ car.py
@@ -4,16 +4,13 @@ class Vehicle:
         self.max_speed = max_speed
         self.number = number
 
-
 class Bus(Vehicle):
     pass
-
 
 def main():
     model = Bus('New Flyer', 65, 9345)
     print(f"{model.number} is a {model.name}.")
     print(f"It is governed at {model.max_speed} MPH!")
-
 
 if __name__ == "__main__":
     main()
