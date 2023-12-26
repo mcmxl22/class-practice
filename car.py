3d59077d9b39942b64bce42dae951687e52e3ff9
M. McConnaughey <mcmxl@hotmail.com>
2023-12-26 12:24:55 -0700

    Update car.py

--- car.py
+++ car.py
@@ -8,11 +8,3 @@ class Vehicle:
 
 class Bus(Vehicle):
     pass
-
-def main():
-    model = Bus('New Flyer', 65, 9345)
-    print(f"{model.number} is a {model.name}.")
-    print(f"It is governed at {model.max_speed} MPH!")
-
-if __name__ == "__main__":
-    main()
